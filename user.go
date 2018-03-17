fc8e678934d7aeb0e3a1e2864a47ac1baa4097a1
Luís <luis_sacco@hotmail.com>
2018-03-17 17:48:42 +0100

    working on register user

--- user.go
+++ user.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	//"net/http"
 )
 
+type SIGNUP struct{
+    USER string `json:"user" binding:"required"`
+    PASSWORD string `json:"password" binding:"required"`
+    EMAIL string `json:"password" binding:"required"`
+    UNIVERSITY string `json:"password" binding:"required"`
+}
+
 func setupUserRoutes(router *gin.Engine) { // Changed *gin.Router to *gin.Context
-	router.GET("/user/:user", getUser)
+	router.GET("/user", getUser)
 	router.POST("/user", registerUser)
 	router.PUT("/user", updateUser)
 }
@@ -14,11 +21,14 @@ func setupUserRoutes(router *gin.Engine) { // Changed *gin.Router to *gin.Contex
 func getUser(ctx *gin.Context) {
 	//do what has to be done for this endpoint
 	//user repository for data retrieval
-
-	ctx.String(http.StatusOK, "AKAMU GET USER")
 }
 
 func registerUser(ctx *gin.Context) {
+	var signup SIGNUP
+        ctx.BindJSON(&signup)
+        //how to call func in files in sub directories
+        //insertUser()
+        ctx.JSON(200, gin.H{"status": signup.USER})
 	//do what has to be done for this endpoint
 	//user repository for data retrieval
 }
