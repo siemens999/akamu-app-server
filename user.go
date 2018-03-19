33ade5d90caad3ff07ee126b6440f0a68068a093
Luís <luis_sacco@hotmail.com>
2018-03-19 19:22:24 +0100

    started http get user

--- user.go
+++ user.go
@@ -5,12 +5,15 @@ import (
 	"./DBHandler"
 	"net/http"
 	"time"
+	"strconv"
 )
 
 func setupUserRoutes(router *gin.Engine) { 
 
 	//endpoint used to register new users
 	router.POST("/user", registerUser)
+	//endpoint used to fetch the user data
+	router.GET("/user/:id", getUser)
 
 	//TODO implement further endpoints
 }
@@ -25,30 +28,61 @@ func registerUser(ctx *gin.Context) {
 	//The standard akamu json response that will be sent in the http response
 	jsonResponse := AkamuJsonResponse{}
 	//SignUpForm is the struct that contains all information a user must provide to create a new account.
-	var signUpForm DBHandler.SignUpForm
+	signUpForm := DBHandler.SignUpForm{}
 	//SignInResponse is the struct that contain all the information given back to user after login or singup
-	var signInResponse DBHandler.SignInResponse
+	signInResponse := DBHandler.SignInResponse{}
 
 	//grabs data from the http post request and bind it to the signUpForm struct
     ctx.BindJSON(&signUpForm)
 
     //insert user into the Akamu sql database
-    err := DBHandler.InsertUser(ctx, signUpForm, &signInResponse)
+    err := DBHandler.InsertUser(signUpForm, &signInResponse)
 
     if err != nil {
     	jsonResponse.Status = "error"
-		jsonResponse.Message = "Failed inserting user:" + err.Error()
+		jsonResponse.Message = "Failed inserting user: " + err.Error()
 		ctx.JSON(http.StatusInternalServerError, jsonResponse)
 		return
 	}
 	
-
     //TODO: Generate appropriated token
 	signInResponse.Token = DBHandler.AuthToken{"token value", time.Now()}
 
+	//set values in the akamu standard response json
 	jsonResponse.Status = "success"
 	jsonResponse.Message = "register new user successful"
 	jsonResponse.Value = &signInResponse
 
+	//set http response
+    ctx.JSON(http.StatusOK, jsonResponse)
+}
+
+func getUser (ctx *gin.Context) {
+
+	//The standard akamu json response that will be sent in the http response
+	jsonResponse := AkamuJsonResponse{}
+	user := DBHandler.User{}
+
+	//TODO: authentification. Perhaps also check if the user id given is the one used to make the token
+
+	//parse the id string into an int id
+	id , _ := strconv.ParseInt(ctx.Param("id"), 10, 0)
+
+	//select user in the database
+    err := DBHandler.SelectUserById(id, &user)
+
+    if err != nil {
+    	jsonResponse.Status = "error"
+		jsonResponse.Message = "Failed retrieving user from database: " + err.Error()
+		ctx.JSON(http.StatusInternalServerError, jsonResponse)
+		return
+	}
+
+	//set values in the akamu standard response json
+	jsonResponse.Status = "success"
+	jsonResponse.Message = "get user by id successful"
+	jsonResponse.Value = &user
+
+	//set http response
     ctx.JSON(http.StatusOK, jsonResponse)
 }
