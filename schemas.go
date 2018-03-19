657ad0180abf008c1364f662fa61bea7d109003c
Frederik Wegner <onlyknoppas@gmail.com>
2018-03-19 20:05:50 +0100

    Added QuestionSchema.

--- schemas.go
+++ schemas.go
@@ -8,10 +8,30 @@ type UserInfoSchema struct {
 }
 
 type UserSchema struct extends UserInfoSchema {
+	*UserInfoSchema
 	TimeRegistered string; 	`json:"time-registered" binding:"required"`	// datetime format
 	Semester int;			`json:"semester" binding:"required"`
-	verified bool;			`json:"verified" binding:"required"`
-	university string;		`json:"university" binding:"required"`
-	experience int;			`json:"experience" binding:"required"`
-	memorycoins int;		`json:"memorycoins" binding:"required"`
+	Verified bool;			`json:"verified" binding:"required"`
+	University string;		`json:"university" binding:"required"`
+	Experience int;			`json:"experience" binding:"required"`
+	Memorycoins int;		`json:"memorycoins" binding:"required"`
+}
+
+type RoundSchema struct {
+	Pool PoolSchema			`json:"pool" binding:"required"`
+	Questions []struct{
+		Question Question	`json:"question" binding:"required"`
+		AnswerOpponent		`json:"answer-opponent"`
+	}	`json:"questions" binding:"required"`
+}
+
+type QuestionSchema struct {
+	Id uint32				`json:"id" binding:"required"`
+	Authro uint32			`json:"author"`
+	Text string				`json:"text" binding:"required"`
+	Image uint32			`json:"image"`
+	Subject string			`json:"subject" binding:"required"`
+	Pool Pool				`json:"pool" binding:"required"`
+	Answer Answer			`json:"answer" binding:"required"`
+	//should be either multiplechoice answer or textinputanswer.
 }
