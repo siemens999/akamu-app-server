12fac7e1ea67b362d63ab3bb6086eac07b44c030
NiklasFix <niklas.fix.de@gmail.com>
2018-03-19 21:00:09 +0100

    Schemas added (3)

--- schemas.go
+++ schemas.go
@@ -3,11 +3,11 @@ package main
 //import "github.com/golang/protobuf/protoc-gen-go/plugin"
 
 type SignUpFormular struct {
-	Name string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-    Email string `json:"email" binding:"required"`
-    University string `json:"university" binding:"required"`
-    Semester string `json:"semester" binding:"required"`
+	Name string        `json:"username" binding:"required"`
+	Password string    `json:"password" binding:"required"`
+    Email string       `json:"email" binding:"required"`
+    University string  `json:"university" binding:"required"`
+    Semester string    `json:"semester" binding:"required"`
 }
 type UserInfoSchema struct {
 	Id uint32       `json:"id" binding:"required"`
@@ -44,21 +44,12 @@ type ExplanationSchema struct {
 	Text string `json:"text" binding:"required"`
 }
 
-type UserSchema struct {
-	TimeRegistered string; 	`json:"time-registered" binding:"required"`	// datetime format
-	Semester int;           `json:"semester" binding:"required"`
-	Verified bool;          `json:"verified" binding:"required"`
-	University string;      `json:"university" binding:"required"`
-	Experience int;         `json:"experience" binding:"required"`
-	Memorycoins int;        `json:"memorycoins" binding:"required"`
-}
-
 type RoundSchema struct {
-	Pool PoolSchema        `json:"pool" binding:"required"`
+	Pool PoolSchema                   `json:"pool" binding:"required"`
 	Questions []struct{
-		Question Question  `json:"question" binding:"required"`
-		AnswerOpponent     `json:"answer-opponent"`
-	}                      `json:"questions" binding:"required"`
+		Question QuestionSchema       `json:"question" binding:"required"`
+		AnswerOpponent AnswerSchema   `json:"answer-opponent"`
+	}                                 `json:"questions" binding:"required"`
 }
 
 type QuestionSchema struct {
@@ -67,22 +58,22 @@ type QuestionSchema struct {
 	Text string             `json:"text" binding:"required"`
 	Image uint32            `json:"image"`
 	Subject string          `json:"subject" binding:"required"`
-	Pool Pool               `json:"pool" binding:"required"`
-	Answer Answer           `json:"answer" binding:"required"`
+	Pool PoolSchema         `json:"pool" binding:"required"`
+	Answer AnswerSchema     `json:"answer" binding:"required"`
 	//should be either multiplechoice answer or textinputanswer.
 }
 
 type DuelInfoSchema struct {
-	Id uint32               `json:"id" binding:"required"`
-	UserChallanger UserInfo `json:"user-challanger" binding:"required"`
-	UserChallanged UserInfo `json:"user-challanged" binding:"required"`
-	Status int              `json:"status" required:"true"`
-	TimeStart string        `json:"time-start" required:"true"`
-	TimeChanged string      `json:"time-changed" required:"true"`
-	TimeEnd string          `json:"time-end"`
-	ScoreChallenger         `json:"score-challanger`
-	ScoreChallenged         `json:"score-challanged`
-	Winner uint32           `json:"winner"`
+	Id uint32                       `json:"id" binding:"required"`
+	UserChallanger UserInfoSchema   `json:"user-challanger" binding:"required"`
+	UserChallanged UserInfoSchema   `json:"user-challanged" binding:"required"`
+	Status int                      `json:"status" required:"true"`
+	TimeStart string                `json:"time-start" required:"true"`
+	TimeChanged string              `json:"time-changed" required:"true"`
+	TimeEnd string                  `json:"time-end"`
+	ScoreChallenger UserInfoSchema  `json:"score-challenger"`
+	ScoreChallenged UserInfoSchema  `json:"score-challenged"`
+	Winner uint32                   `json:"winner"`
 }
 
 type DuelSchema {
@@ -98,6 +89,8 @@ type MultipleChoiceAnswerItemSchema struct {
 	Text string      `json:"text" binding:"required"`
 }
 
-type TextInputAnswer string
+type TextInputAnswerSchema string
 
-// type Answer struct {} // TODO: implement this!
+type AnswerSchema struct {
+	// TODO: implement this!
+}
